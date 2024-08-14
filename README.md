b4e0543c810cb6341353d8c751487683d5330393
Lenivaya Koala <Kridekrok@yandex.ru>
2024-08-14 16:16:16 +0500

    Версия 1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/LenivauyKoala/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardTemplate = document.querySelector('#card-template').content;
+const containerCard = document.querySelector('.places__list');
+
+addCard(containerCard, initialCards);
+
+function addCard(containerCard, initialCards) {
+  for (i = 0; i < initialCards.length; i++) {
+    containerCard.append(creationsCard(cardTemplate, initialCards[i], deleteCard));
+  };
+}
+
+function creationsCard(cardTemplate, cardInfo, remove) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = cardInfo.name;
+  
+  const cardPictures = cardElement.querySelector('.card__image');
+  cardPictures.src = cardInfo.link;
+  cardPictures.alt = cardInfo.name;
+
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+ 
+  deleteButton.addEventListener('click', () => remove(cardElement));
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
